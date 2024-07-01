ced762ab838ffaecc35553f2bdf75fc4d2f86d3a
MostafaAdly <MostafaAdly.official@gmail.com>
2024-07-01 04:22:01 +0300

    added .gitignore

--- main.go
+++ main.go
@@ -7,8 +7,19 @@ import (
 
 func main() {
 	arr :=
-		List.NewArray([]int{1, 2, 3, 4, 5}).Map(func(value int, index int) int {
-			return value * 2
-		}).Build()
+		List.
+			NewArray([]int{1, 2, 3, 4, 5}).
+			Map(
+				func(value int, index int) int {
+					return value * 2
+				},
+			).
+			Filter(
+				func(value int) bool {
+					return value > 5
+				},
+			).
+			Build()
 	fmt.Println(arr)
+	// Output: [6 8 10]
 }
